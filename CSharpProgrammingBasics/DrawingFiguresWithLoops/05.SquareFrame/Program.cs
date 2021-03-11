cbae5ad8a40798b1d5e03e98a3e206c7da7277b5
iwanmitowski <iwan.mitowski@gmail.com>
2021-03-11 14:53:05 +0200

    Adding the last exercise

--- CSharpProgrammingBasics/DrawingFiguresWithLoops/05.SquareFrame/Program.cs
+++ CSharpProgrammingBasics/DrawingFiguresWithLoops/05.SquareFrame/Program.cs
@@ -6,6 +6,49 @@ namespace _05.SquareFrame
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            string corner = "+";
+            string horizontalBorder = "-";
+            string verticalBorder = "|";
+
+            DrawTopBot(corner, horizontalBorder, n);
+            for (int i = 0; i < n-2; i++)
+            {
+                DrawMiddlePart(verticalBorder, horizontalBorder, n);
+                Console.WriteLine();
+            }
+            DrawTopBot(corner, horizontalBorder, n);
+
         }
+
+        static void DrawTopBot(string corner, string horizontalBorder, int n)
+        {
+            Console.Write(corner);
+            Console.Write(" ");
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write(horizontalBorder);
+                Console.Write(" ");
+
+            }
+            Console.Write(corner);
+            Console.WriteLine();
+
+        }
+
+        static void DrawMiddlePart(string verticalBorder, string horizontalBorder, int n)
+        {
+            Console.Write(verticalBorder);
+            Console.Write(" ");
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write(horizontalBorder);
+                Console.Write(" ");
+
+            }
+            Console.Write(verticalBorder);
+
+        }
+
     }
 }
--- CSharpProgrammingBasics/DrawingFiguresWithLoops/10.Diamond/Program.cs
+++ CSharpProgrammingBasics/DrawingFiguresWithLoops/10.Diamond/Program.cs
@@ -6,7 +6,91 @@ namespace _10.Diamond
     {
         static void Main(string[] args)
         {
-           
+            int n = int.Parse(Console.ReadLine());
+            int leftRight = (n - 1) / 2;
+
+            string dash = "-";
+            string star = "*";
+
+            while (leftRight!=0)
+            {
+                int mid = n - 2 * leftRight - 2;
+
+                for (int j = 0; j < leftRight; j++)
+                {
+                    Console.Write(dash);
+                }
+
+                Console.Write(star);
+
+                for (int k = 0; k < mid; k++)
+                {
+                    Console.Write(dash);
+                }
+
+
+                if (mid >= 0)
+                {
+                    Console.Write(star);
+
+                }
+
+
+                for (int j = 0; j < leftRight; j++)
+                {
+                    Console.Write(dash);
+                }
+                Console.WriteLine();
+                leftRight--;
+            }
+
+
+            int reps;
+
+            if (n%2==0)
+            {
+                reps = n / 2;
+            }
+            else
+            {
+                reps = n / 2 + 1;
+            }
+
+            while (leftRight < reps)
+            {
+                int mid = n - 2 * leftRight - 2;
+
+                for (int j = 0; j < leftRight; j++)
+                {
+                    Console.Write(dash);
+                }
+
+                Console.Write(star);
+
+                for (int k = 0; k < mid; k++)
+                {
+                    Console.Write(dash);
+                }
+
+
+                if (mid >= 0)
+                {
+                    Console.Write(star);
+
+                }
+
+
+                for (int j = 0; j < leftRight; j++)
+                {
+                    Console.Write(dash);
+                }
+                Console.WriteLine();
+                leftRight++;
+            }
+
+
         }
+
     }
 }
+
