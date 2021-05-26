23379b29f608ebdd62d7c5ed12a48670c8f7341e
iwanmitowski <iwan.mitowski@gmail.com>
2021-05-26 15:10:37 +0300

    Adding Data types and variables exercise task and 2 of the more exercise tasks

---
+++ Fundamentals/DataTypesAndVariablesExercises/DataTypesAndVariablesExercise/07.WaterOverflow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07.WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentWater = int.Parse(Console.ReadLine());
+
+                if (currentWater+sum>255)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                else
+                {
+                    sum += currentWater;
+                }
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Fundamentals/DataTypesAndVariablesExercises/DataTypesAndVariablesExercise/08.BeerKegs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _08.BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string biggestModel = string.Empty;
+            double biggestSize = 0;
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double currentSize = Math.PI * (radius*radius) * height;
+
+                if (currentSize>biggestSize)
+                {
+                    biggestModel = model;
+                    biggestSize = currentSize;
+                }
+
+            }
+
+            Console.WriteLine(biggestModel);
+        }
+    }
+}
