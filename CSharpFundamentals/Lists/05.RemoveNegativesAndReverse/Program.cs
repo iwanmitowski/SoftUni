b4576e691288bd1b7ce73b5d0b7673328377b5ff
iwanmitowski <iwan.mitowski@gmail.com>
2021-06-17 21:14:45 +0300

    Adding list lab problems

---
+++ CSharpFundamentals/Lists/05.RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.RemoveNegativesAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).Where(x => x >= 0).ToList();
+
+            numbers.Reverse();
+
+            string result = numbers.Count == 0 ? "empty" : string.Join(" ", numbers);
+
+            Console.WriteLine(result);
+        }
+    }
+}
