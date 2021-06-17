4bf57f5e0ce24ada2f38a52b092236af90548da1
iwanmitowski <iwan13mitowski@gmail.com>
2021-06-17 21:03:12 +0300

    Adding list exercises folder

---
+++ CSharpFundamentals/ListExercises/ListsExercises/01.Train/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> train = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int maxCapacity = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+
+            while (command!="end")
+            {
+                if (command.Contains("Add"))
+                {
+                    string[] tokens = command.Split();
+
+                    train.Add(int.Parse(tokens[1]));
+                }
+                else
+                {
+                    int currentPassangers = int.Parse(command);
+
+                    for (int i = 0; i < train.Count; i++)
+                    {
+                        if (train[i]+ currentPassangers<=maxCapacity)
+                        {
+                            train[i] += currentPassangers;
+                            break;
+                        }
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+
+            Console.WriteLine(string.Join(" ",train));
+
+        }
+    }
+}
