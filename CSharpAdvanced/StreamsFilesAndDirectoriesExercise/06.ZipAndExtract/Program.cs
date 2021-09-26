f531d46889dedfff1f78175472526fd08dc8d474
iwanmitowski <iwan.mitowski@gmail.com>
2021-09-26 10:42:44 +0300

    Adding brackets so the program doesn't throw exception

--- CSharpAdvanced/StreamsFilesAndDirectoriesExercise/06.ZipAndExtract/Program.cs
+++ CSharpAdvanced/StreamsFilesAndDirectoriesExercise/06.ZipAndExtract/Program.cs
@@ -11,8 +11,10 @@ namespace _06.ZipAndExtract
             string filePath = Path.Combine("..", "..", "..", "copyMe.png");
             string zipPath = Path.Combine("..", "..", "..", "archive.zip");
 
-            using ZipArchive zip = ZipFile.Open(zipPath, ZipArchiveMode.Create);
-            zip.CreateEntryFromFile(filePath, "copyMe.png");
+            using (ZipArchive zip = ZipFile.Open(zipPath, ZipArchiveMode.Create))
+            {
+                zip.CreateEntryFromFile(filePath, "copyMe.png");
+            };
 
             string extractPath = Path.Combine("..", "..", "..", "extracted");
             ZipFile.ExtractToDirectory(zipPath, extractPath);
