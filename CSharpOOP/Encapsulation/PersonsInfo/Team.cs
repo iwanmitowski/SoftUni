a0b347afd7f3ae2a7c9957d5a6df3d71375d9eb6
iwanmitowski <iwan.mitowski@gmail.com>
2021-10-31 11:52:06 +0200

    Adding Encapsulation lab

---
+++ CSharpOOP/Encapsulation/PersonsInfo/Team.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private readonly List<Person> firstTeam;
+        private readonly List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            Name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+        public string Name { get => name; set => name = value; }
+        public IReadOnlyCollection<Person> FirstTeam => firstTeam.AsReadOnly();
+        public IReadOnlyCollection<Person> ReserveTeam => reserveTeam.AsReadOnly();
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"First team has {firstTeam.Count} players.");
+            sb.AppendLine($"Reserve team has {reserveTeam.Count} players.");
+
+            return sb.ToString();
+        }
+    }
+}
