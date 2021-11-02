c56cfdf64597cdf14cfa895788cd56fee03366fe
iwanmitowski <iwan.mitowski@gmail.com>
2021-11-02 22:07:03 +0200

    Adding half of the Encapsulation Exercise

---
+++ CSharpOOP/EncapsulationExercise/ClassBoxData/Box.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace _01.ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get => length;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(Length)} cannot be zero or negative.");
+                }
+
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => width;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(Width)} cannot be zero or negative.");
+                }
+
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => height;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(Height)} cannot be zero or negative.");
+                }
+
+                height = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Surface Area - {SurfaceArea():f2}");
+            sb.AppendLine($"Lateral Surface Area - {LateralSurfaceArea():f2}");
+            sb.AppendLine($"Volume - {Volue():f2}");
+
+            return sb.ToString().Trim();
+        }
+
+        public double SurfaceArea()
+        {
+            return 2 * Length * Width + 2 * Length * Height + 2 * Width * Height;
+        }
+
+        public double LateralSurfaceArea()
+        {
+            return 2 * Length * Height + 2 * Width * Height; 
+        }
+
+        public double Volue()
+        {
+            return Length * Width * Height;
+        }
+    }
+}
---
+++ CSharpOOP/EncapsulationExercise/ClassBoxData/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01.ClassBoxData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double c = double.Parse(Console.ReadLine());
+
+                var box = new Box(a, b, c);
+
+                Console.WriteLine(box);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+        }
+    }
+}
