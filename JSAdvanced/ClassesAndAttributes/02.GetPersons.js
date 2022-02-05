5846193b43934a428c95c584b09046170ce8ad7e
iwanmitowski <iwan.mitowski@gmail.com>
2022-02-05 20:23:36 +0200

    Adding Classes And Attributes lab

---
+++ JSAdvanced/ClassesAndAttributes/02.GetPersons.js
@@ -0,0 +1,28 @@
+function getPeople(){
+    class Person{
+        constructor(firstName, lastName, age, email){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+        }
+    
+        toString(){
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+        }
+    }
+
+    let people = [
+        new Person('Anna', 'Simpson', 22, 'anna@yahoo.com'),
+        new Person('SoftUni'),
+        new Person('Stephan', 'Johnson', 25),
+        new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com'),
+        
+    ]
+
+    console.log(people[0].toString());
+
+    return people;
+}
+
+getPeople();
---
+++ JSAdvanced/ClassesAndAttributes/03.Circle.js
@@ -0,0 +1,28 @@
+class Circle{
+    
+    constructor(radius){
+        this.radius = radius;
+    }
+
+    get diameter(){
+        return this.radius * 2;
+    }
+
+    set diameter(value){
+        this.radius = value / 2;
+    }
+
+    get area(){
+        let test = Math.PI * this.radius ** 2;
+        return test;
+    }
+}
+
+let c = new Circle(2);
+console.log(`Radius: ${c.radius}`);
+console.log(`Diameter: ${c.diameter}`);
+console.log(`Area: ${c.area}`);
+c.diameter = 1.6;
+console.log(`Radius: ${c.radius}`);
+console.log(`Diameter: ${c.diameter}`);
+console.log(`Area: ${c.area}`);
